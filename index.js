6ad23aacaa7bcca67d8c001a718b288aa251729d
Matthew Bartl <mattbartl@gmail.com>
2019-02-24 23:36:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandpa(string) {
+function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   } if (string.toUpperCase() === string) {
